36a3e89466a652214bb0df824f269e87140357bf
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-03 15:14:28 +0200

    correction for the sorting code

--- sorting.js
+++ sorting.js
@@ -1,5 +1,12 @@
 function sortingCards(cardsDeck) {
-  return cardsDeck.sort();
-
-  // I couldn't think of a way to implement in Javascript and the one I made isn't working
+  for (var i = 0; i < cardsDeck.length; i++) {
+    for (var j = 0; j < cardsDeck.length - i - 1; j++) {
+      if (cardsDeck[j] > cardsDeck[j + 1]) {
+        var temp = cardsDeck[j];
+        cardsDeck[j] = cardsDeck[j + 1];
+        cardsDeck[j + 1] = temp;
+      }
+    }
+  }
+  console.log(cardsDeck);
 }
